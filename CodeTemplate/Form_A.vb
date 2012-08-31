25e3ca9727e5765f7851b707d1ce4a000f0b1393
matsuno18 <matsuno18@live.com>
2012-08-31 16:16:32 +0900

    初期作成

---
+++ CodeTemplate/Form_A.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form_A
+
+#Region "サンプル実装"
+    ''' <summary>
+    ''' 他スレッドからメッセージボックスを表示させる
+    ''' </summary>
+    ''' <param name="message_text"></param>
+    ''' <param name="dialog_caption"></param>
+    ''' <remarks>他スレッドからGUIを操作する方法のサンプル実装です</remarks>
+    Public Sub ShowMsgBox(ByVal message_text As String, ByVal dialog_caption As String)
+        Invoke(New Action(Of String, String)(AddressOf _ShowMsgBox), message_text, dialog_caption)
+    End Sub
+    Private Sub _ShowMsgBox(ByVal message_text As String, ByVal dialog_caption As String)
+        MsgBox(message_text, MsgBoxStyle.Information, dialog_caption)
+    End Sub
+#End Region
+
+End Class
---
+++ CodeTemplate/Main.vb
@@ -0,0 +1,59 @@
+﻿Module Main
+
+    Sub Main(ByVal CmdArgs() As String)
+
+        Dim myApp As New MyAppBase
+        myApp.Run(CmdArgs)
+
+    End Sub
+
+End Module
+
+''' <summary>
+''' 
+''' </summary>
+''' <remarks>
+''' Visual Basic アプリケーションモデルを利用するために用意されたクラス
+''' </remarks>
+Class MyAppBase
+    Inherits Microsoft.VisualBasic.ApplicationServices.WindowsFormsApplicationBase
+
+    Sub New()
+        '' VBアプリケーションモデルの初期化・設定をする
+        ''=====================================================================
+
+        ' ビジュアルスタイルを有効にする
+        Me.EnableVisualStyles = True
+
+        ' 単一インスタンスのアプリケーションとする
+        Me.IsSingleInstance = True
+
+        ' 上記と関連して，２重起動されたときに呼ばれるイベントを設定する
+        AddHandler Me.StartupNextInstance, AddressOf MyAppBase_StartupNextInstance
+
+        ' このアプリケーションのメインとなるFormのインスタンスを設定する
+        Me.MainForm = New Form_A()
+
+    End Sub
+
+    ' ２重起動されたときに呼ばれる処理
+    Private Sub MyAppBase_StartupNextInstance(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs)
+        ''[概要]
+        '' ウィンドウをアクティブにして，さらに２重起動されたことを通知する
+        ''=====================================================================
+
+        ' ウィンドウのZオーダーを最前面となるように設定する
+        Me.MainForm.BringToFront()
+
+        ' アプリケーションをアクティブにする(アクティブにする対象は自分自身)
+        AppActivate(Process.GetCurrentProcess.Id)
+
+        ' このアプリケーションのメインフォームをアクティブにする
+        Me.MainForm.Activate()
+
+        ' メッセージを表示する
+        MsgBox("２重起動されました！", MsgBoxStyle.Information, "通知")
+
+    End Sub
+
+End Class
